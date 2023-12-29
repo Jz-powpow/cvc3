0ec1ee819571b3bdf8337dbd0c64ac6c25f38288
Jz-powpow <113581514+Jz-powpow@users.noreply.github.com>
2023-12-29 15:42:37 -0300

    bin.php

--- gates/bin.php
+++ gates/bin.php
@@ -3,34 +3,26 @@ if ((strpos($message, "/bin") === 0)||(strpos($message, "!bin") === 0)||(strpos(
   $message = substr($message, 5);
 $message = clean($message);
 $bin = substr($message,0,6);
-  $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://bins.su/');
-    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    $headers = array();
-    $headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
-    $headers[] = 'Accept-Language: en-US,en;q=0.9';
-    $headers[] = 'Cache-Control: max-age=0';
-    $headers[] = 'Connection: keep-alive';
-    $headers[] = 'Content-Type: application/x-www-form-urlencoded';
-    $headers[] = 'Host: bins.su';
-    $headers[] = 'Origin: http://bins.su';
-    $headers[] = 'Referer: http://bins.su/';
-    $headers[] = 'Upgrade-Insecure-Requests: 1';
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, 'action=searchbins&bins='.$bin.'&bank=&country=');
-$result = curl_exec($ch);
-    $bincap1 = trim(strip_tags(getStr($result, '<td>Bank</td></tr><tr><td>','</td>')));
-    $country = (getStr($result, '<td>'.$bincap1.'</td><td>','</td>'));
-    $brand = trim(strip_tags(getStr($result, '<td>'.$country.'</td><td>','</td>')));
-    $level = trim(strip_tags(getStr($result, '<td>'.$brand.'</td><td>','</td>')));
-    $type = trim(strip_tags(getStr($result, '<td>'.$level.'</td><td>','</td>')));
-    $bank = trim(strip_tags(getStr($result, '<td>'.$type.'</td><td>','</td>')));
-  if((empty($bank)) && (empty($country)) && (empty($brand)) && (empty($type)))
+  $ch = $curl = curl_init();
+    
+curl_setopt_array($curl, array(
+CURLOPT_URL => 'https://zylalabs.com/api/3100/bin+lookup+api/3286/bin+checker?bin=373723',
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => '',
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 0,
+CURLOPT_FOLLOWLOCATION => true,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => 'GET',
+CURLOPT_HTTPHEADER => array(
+    'Authorization: Bearer 3112|PlsFKPeSgfWiKlvcXHGBs8xirjelH1wYrjNZMRg5'
+    ),
+));
+    
+$response = curl_exec($curl);
+    
+curl_close($curl);
+echo $response;
   {
     $msg = "𝗡𝗢𝗧 𝗩𝗔𝗟𝗜𝗗 ❌";
     goto binn;
@@ -67,4 +59,22 @@ $header
  reply_to($chatId,$msg,$messageId);
 
 }
+
+ "status": 200,
+  "success": true,
+  "isValid": true,
+  "message": "The BIN number is valid.",
+  "data": {
+    "bin_iin": "373723",
+    "card_brand": "AMERICAN EXPRESS",
+    "card_type": "CREDIT",
+    "card_level": "OPTIMA",
+    "issuer_name_bank": "AMERICAN EXPRESS US CONSUMER",
+    "issuer_bank_website": "------",
+    "issuer_bank_phone": "------",
+    "iso_country_name": "UNITED STATES",
+    "iso_country_code": "US"
+  }
+}
+  
 ?>
