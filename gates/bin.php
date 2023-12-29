913079dedb9a7e349922d002c1fa928b418121a8
Jz-powpow <113581514+Jz-powpow@users.noreply.github.com>
2023-12-29 15:50:03 -0300

    bin.php

--- gates/bin.php
+++ gates/bin.php
@@ -1,53 +1,54 @@
 <?php 
-}if(strpos($text,"/bin") !== false){ 
-$bin = trim(str_replace("/bin","",$text)); 
+//Bin Lookup
+if(strpos($message, "!bin") === 0){
+    $bin = substr($message, 5);
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "https://binsu-api.vercel.app/api/".$bin,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+    "sec-fetch-dest: document",
+    "sec-fetch-site: none",
+    "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+   ],
+   ]);
 
-$data = json_decode(file_get_contents("https://bins-su-api.vercel.app/api/$bin"),true);
-$bank = $data['data']['bin'];
-$vendor =  $data['data']['vendor'];
-$type =  $data['data']['type'];
-$level =  $data['data']['level'];
-$bank =  $data['data']['bank'];
-$country =  $data['data']['country'];
+ $result = curl_exec($curl);
+ curl_close($curl);
+ $data = json_decode($result, true);
+ $bank = $data['data']['bank'];
+ $country = $data['data']['country'];
+ $brand = $data['data']['vendor'];
+ $level = $data['data']['level'];
+ $type = $data['data']['type'];
+ $flag = $data['data']['countryInfo']['emoji'];
+ $result1 = $data['result'];
 
- if($data['data']){
-bot('sendmessage', [
-                'chat_id' =>$chat_id,
-                'text' =>"***
-  {
-    $msg = "𝗡𝗢𝗧 𝗩𝗔𝗟𝗜𝗗 ❌";
-    goto binn;
-  }
-if(empty($bank))
-{
-  $bank = "N/A";
+    if ($result1 == true) {
+    send_message($chat_id,$message_id, "***✅ Valid BIN
+Bin: $bin
+Brand: $brand
+Level: $level
+Bank: $bank
+Country: $country $flag
+Type:$type
+Checked By @$username ***");
+    }
+else {
+    send_message($chat_id,$message_id, "***Enter Valid BIN***");
 }
-  if(empty($country))
-{
-  $country = "N/A";
 }
-  if(empty($brand))
-{
-  $brand = "N/A";
-}
-  if(empty($type))
-{
-  $type = "N/A";
-}
-$msg = urlencode(
-  "
-𝗩𝗔𝗟𝗜𝗗 𝗕𝗜𝗡 ✅
- 
-➜ 𝘽𝙄𝙉: $bin - $brand
-➜ 𝘽𝘼𝙉𝙆: $bank
-➜ 𝘾𝙊𝙐𝙉𝙏𝙍𝙔: $country
-➜ 𝙏𝙔𝙋𝙀: $type 
-
-$header
-𝗕𝗢𝗧 𝗕𝗬 - $bowner"
-);
-  binn:
- reply_to($chatId,$msg,$messageId);
-
-}
+    function send_message($chat_id,$message_id, $message){
+        $text = urlencode($message);
+        $apiToken = $_ENV['API_TOKEN'];  
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=Markdown");
+    }
 ?>
