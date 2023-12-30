0bbfbb5aaaf59ef66be2244a3b522d98e7c7d30c
Jz-powpow <113581514+Jz-powpow@users.noreply.github.com>
2023-12-30 03:09:05 -0300

    bin.php

--- gates/bin.php
+++ gates/bin.php
@@ -2,6 +2,11 @@
 if ((strpos($message, "/bin $bin") === 0)||(strpos($message, "!bin $bin") === 0)||(strpos($message, ".bin $bin") === 0)){
 
 
+$mail = random_strings(5)."@gmail.com";
+//---------------------------------------//
+# -------------------- [PROXY SECTION] -------------------#
+
+//---------------------------------------//
 $upd = urlencode("
 ➜ 𝘾𝙃𝙀𝘾𝙆𝙄𝙉𝙂 𝙎𝙏𝘼𝙍𝙏𝙀𝘿...
 ");
@@ -9,10 +14,7 @@ $sss = reply_to($chatId,"$upd",$messageId);
 $respon = json_decode($sss, TRUE);
 $message_id_1 = $respon['result']['message_id'];
 
-  sendaction($chatId, typing);
-$bin = substr($message, 5);
-$bin = substr("$bin", 0, 6);
-$ch = curl_init();
+  $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://lookup.binlist.net/'.$bin.'');
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -21,21 +23,30 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'));
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'bin='.$bin.'');
+curl_setopt($ch, CURLOPT_POSTFIELDS, '');
 $fim = curl_exec($ch);
 $bank = GetStr($fim, '"bank":{"name":"', '"');
-$name = strtoupper(GetStr($fim, '"name":"', '"'));
-$brand = strtoupper(GetStr($fim, '"brand":"', '"'));
-$country = strtoupper(GetStr($fim, '"country":{"name":"', '"'));
-$scheme = strtoupper(GetStr($fim, '"scheme":"', '"'));
+$name = GetStr($fim, '"name":"', '"');
+$brand = GetStr($fim, '"brand":"', '"');
+$country = GetStr($fim, '"country":{"name":"', '"');
 $emoji = GetStr($fim, '"emoji":"', '"');
-$type =strtoupper(GetStr($fim, '"type":"', '"'));
-if(strpos($fim, '"type":"Credit"') !== false){
-};
-  $time_end = microtime(true);
-  $execution_time = ($time_end - $time_start);
-sendMessage($chatId, "<b>HexnyneCHK - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] TOOL: BIN LOOKUP%0A[ϟ] STATUS: VALID BIN ✅%0A[ϟ] BIN: $bin%0A[ϟ] COUNTRY: $country $emoji%0A[ϟ] BRAND: $brand%0A[ϟ] LEVEL: $scheme%0A[ϟ] TYPE: $type%0A[ϟ] BANK: $bank%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>", $message_id);
+$scheme = GetStr($fim, '"scheme":"', '"');
+$type = GetStr($fim, '"type":"', '"');
+$currency = GetStr($fim, '"currency":"', '"');
+///$test2 = GetStr($fim, '"alpha2":"', '"'); ////country abbreviated example (US)
+if(strpos($fim, '"type":"credit"') !== false){
+$bin = 'Credit';
+}else{
+$bin = 'Debit';
 }
+curl_close($ch);
+{
+  $upd = urlencode("
+𝙒𝘼𝙄𝙏𝙄𝙉𝙂 𝙁𝙊𝙍 𝙍𝙀𝙎𝙐𝙇𝙏𝙎...
+");
+edit_message($chatId,"$upd ",$message_id_1);
+  exit();
+  }
 
   
   ?>
